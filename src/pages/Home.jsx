5d84d7b5129fa6d3b03008140c7ef9cfe716e9a3
Vitaly <w.182@mail.ru>
2022-05-04 23:34:37 +0300

    announcement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Announcement from '../components/Announcement'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Announcement/>
+            <Navbar/>
         </div>
     )
 }
